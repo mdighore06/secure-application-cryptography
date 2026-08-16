269d566506ff4e7cbbfc5f4abf81acbcb680420f
mdighore06 <dighoremayur18@gmail.com>
2026-08-16 20:27:47 +0530

    Update _init_.py

--- app/_init_.py
+++ app/_init_.py
@@ -1,1 +1,11 @@
+from flask import Flask
+from app.security import SECRET_KEY
 
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = SECRET_KEY
+
+    from app.routes import bp
+    app.register_blueprint(bp)
+
+    return app
