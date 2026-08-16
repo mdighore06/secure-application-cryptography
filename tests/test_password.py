2017577595a652b3c6fbea8a61531a3692ad9288
mdighore06 <dighoremayur18@gmail.com>
2026-08-16 20:34:53 +0530

    Update test_password.py

--- tests/test_password.py
+++ tests/test_password.py
@@ -1,1 +1,18 @@
+import pytest
+from crypto.hash_password import hash_password, verify_password
 
+def test_hash_uniqueness():
+    pwd = "SecureTestPassword123!"
+    h1 = hash_password(pwd)
+    h2 = hash_password(pwd)
+    assert h1 != h2
+
+def test_verification_success():
+    pwd = "SecureTestPassword123!"
+    h = hash_password(pwd)
+    assert verify_password(pwd, h) is True
+
+def test_verification_failure():
+    pwd = "SecureTestPassword123!"
+    h = hash_password(pwd)
+    assert verify_password("WrongPassword", h) is False
